4376e78db10988f53370bfc4c00fc7e82c051fbe
altrono <hbotema16@gmail.com>
2023-01-31 12:29:13 +0200

    Added Canvas Widget and Images including styling

--- main.py
+++ main.py
@@ -19,5 +19,17 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title('Pomodoro')
+window.config(padx=100, pady=50, bg=YELLOW,)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+photo_image = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=photo_image)
+
+
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+canvas.pack()
+
 
 window.mainloop()
