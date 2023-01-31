72df6d02e37f63261c21e0a18f913fa55850a55c
altrono <hbotema16@gmail.com>
2023-01-31 19:07:30 +0200

    Completed count down mechanism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,13 +12,16 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(500)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+    count_min = math.floor(count /60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        canvas.itemconfig(timer_text, text=count)
-        print(f'Na doba {count}x niar wana fort')
         window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -35,9 +39,9 @@ photo_image = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=photo_image)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
-count_down(10)
 
-start_button = Button(text="Start", highlightthickness=0)
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
