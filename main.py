bc36ca76a6536852eb0fb6cec0dd3c7e51cc60fe
altrono <hbotema16@gmail.com>
2023-01-31 17:10:41 +0200

    Completed the UI of the project with other widget like Label , button

--- main.py
+++ main.py
@@ -20,16 +20,27 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW,)
+text = "✓"
+
+title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
+title_label.grid(column=1, row=0)
+
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 photo_image = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=photo_image)
-
-
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
-canvas.pack()
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text=text, fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
 
 
 window.mainloop()
